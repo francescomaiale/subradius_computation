3770b6c6b3d4312cbcc592b3c5fa9aceef902f73
Francesco Paolo Maiale <53312837+francescomaiale@users.noreply.github.com>
2024-06-10 12:04:15 +0200

    Update illustrative_example.m

--- illustrative_example.m
+++ illustrative_example.m
@@ -1,66 +1,53 @@
-% In this file, we consider the example of Section 5.1 (Illustrative Example) and apply all our algorithms: Algorithm S (lsr_comp_standard.m), Algorithm A (adaptive_subradius_comp.m), and Algorithm E (adaptive_eigenvectors_subradius_comp.m)
+% In this file, we consider the example of Section 5.1 (Illustrative Example) and apply all algorithms: 
+% Algorithm S (lsr_comp_standard.m), Algorithm A (adaptive_subradius_comp.m), and Algorithm E (adaptive_eigenvectors_subradius_comp.m)
 
 close all
 clearvars
 
-%% Setting of the example
-
 A_1 = [7 2; 0 3]; A_2 = [2 0; 4 8];
-
 A = [A_1 A_2];
 
-Pi = A_1*A_2*(A_1^2*A_2)^2; % s.l.p. for the family
+Pi = A_1*A_2*(A_1^2*A_2)^2; % known s.l.p. for the family 
+rho = eigs(Pi,1,'largestabs')^(1/8); % lsr = spectral radius(s.l.p.)^(1 / degree(s.l.p.))
 
-rho = eigs(Pi,1,'largestabs')^(1/8); % lsr of the family computed as rho(s.l.p.)^(1 / degree(s.l.p.))
+% For simplicity and to avoid large entries appearing when we let the algorithms investigate
+% very long products, we apply our algorithms to the renormalized family:
+A = A/rho; 
 
-A = A/rho; % We consider the normalized family for simplicity and to avoid large entries appearing when we let the algorithms investigate very long products.
-
-
-% Setting parameters for the algoritms: delta (accuracy), numeric_error = 0 (no numerical error in the entries of A) and display = 1 (to show the progress after each step)
-delta = 1e-6; numeric_error = [0 0]; display = 1;
-
+delta = 1e-6; % pre-selected accuracy to which we want to approximate the lsr
+numeric_error = [0 0]; % since there is no numerical error on the entries of A
+display = 1; % to show the progress of the algorithms after each step; set display=0 otherwise
+V_in = eye(2); % 1-antinorm is the initial polytope antinorm in all cases
 
 
 %% Algorithm S (Algorithm 4.1 in the main paper)
-
-M=[500 5000 10000]; % Different values of maximum number of allowed antinorm evaluations for comparison
-
-V_in = eye(2); % 1-antinorm as the polytope antinorm
+M=[500 5000 10000]; % different values of maximum number of allowed antinorm evaluations
 
 for i = 1:length(M)
     [lsr_standard(i,:),p_standard(i,:),~] = lsr_comp_standard(A, numeric_error, delta, M(i), V_in, [], display);
 end
 
 
-
-%% Algorithm A with initial antinorm the 1-antinorm
-
+%% Algorithm A
 M = [100 500 1000];
-
-V_adaptive_A = []; % To store the adaptive antinorms obtained at the end of the algorithm for all values of M
+n_vert_A = zeros(length(M),1); % store the number of vertices of the adaptive antinorms
+V_adaptive_A = []; % store the vertices of the adaptive antinorms
 
 for i = 1:length(M)
-
     [lsr_adaptive_A(i,:),p_adaptive_A(i,:),~,V_new] = adaptive_subradius_comp(A, numeric_error, delta, M(i), V_in, [], display);
-    
-    V_adaptive_A = [V_new zeros(2,1)]; % The column (0, 0)^T is added to separate the different vertices set obtained for different values of M.
-
+    V_adaptive_A = [V_new zeros(2,1)]; % the column (0, 0)^T is added to separate the different vertices set obtained for different values of M.
+    n_vert_A(i) = size(V_new,2);
 end
 
 
-
-%% Algorithm E with initial antinorm the 1-antinorm
-
-M = [50 500 1000];
-
-V_adaptive_E = []; % To store the adaptive antinorms obtained at the end of the algorithm for all values of M
-
-theta = 1.005; % The scaling parameter (to rescale eigenvectors prior to adding them)
+%% Algorithm E
+M = [100 500 1000];
+n_vert_E = zeros(length(M),1); % store the number of vertices of the adaptive antinorms
+V_adaptive_E = []; % store the adaptive antinorms
+theta = 1.005; % scaling parameter (to rescale eigenvectors prior to adding them)
 
 for i = 1:length(M)
-
     [lsr_adaptive_E(i,:),p_adaptive_E(i,:),~,V_new] = adaptive_eigenvectors_subradius_comp(A, numeric_error, delta, M(i), V_in, [], display,theta);
-    
     V_adaptive_E = [V_new zeros(2,1)];
-
+    n_vert_E(i) = size(V_new,2);
 end
