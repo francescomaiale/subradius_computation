22d3b3287dd1e535ce39bd7f6813eb87bd0069bf
Francesco Paolo Maiale <53312837+francescomaiale@users.noreply.github.com>
2024-06-10 14:02:19 +0200

    Update real_antinorm.m

--- real_antinorm.m
+++ real_antinorm.m
@@ -4,19 +4,19 @@ function [lower,upper,x] = real_antinorm(V,z)
 
 
 %% Input
-% V is a dxp real matrix which contains the p vertices defining the polytope antinorm as columns.
-% z is a real vector which belongs to the cone on which the antinorm is defined.
+% V is a dxp real matrix which contains the p vertices defining the polytope antinorm as columns
+% z is a real vector which belongs to the cone on which the antinorm is defined
 
 %% Output
-% The values lower and upper provide a lower and upper bound for the value of the antinorm of z.
-% The vector x consists of 1/a(z) as its first entry, and the values of the variables in the constraints of the LP problem in all other entries.
+% The values lower and upper provide a lower and upper bound for the value of the antinorm of z
+% The vector x consists of 1/a(z) as its first entry, and the values of the variables in the constraints of the LP problem in all other entries
 
 
 
 [d,p]=size(V); % d = dimension, p = number of vertices
-A=[-z V;0 -ones(1,m)]; % Augmented matrix as discussed in Algorithm SM.1.1
+A=[-z V;0 -ones(1,m)]; % augmented matrix as defined in Algorithm SM.1.1
 
-% The vector c = (1,0,...,0) is used since linprog solves min c^T x, which would be exactly x(1) in this case.
+% The vector c = (1,0,...,0) is used since linprog solves min c^T x, which would be exactly x(1) in this case
 c = zeros(m+1,1); c(1,1) = 1;
 
 % The vector b is used to take care of the inequality constraints in the LP problem
@@ -32,19 +32,19 @@ options=optimset('MaxIter',max(300,n),'DISPLAY','off','TolFun',tol);
 % x solution of the LP problem using linprog, EXITFLAG is also returned as it gives information in case an error should appear
 [x,~,EXITFLAG,~,~]=linprog(c,A,b,Aeq,beq,zeros(m+1,1),[],options);
 
-if (EXITFLAG<0) % If EXITFLAG is less than 0, then there was an error.
+if (EXITFLAG<0) % If EXITFLAG is less than 0, then there is an error
     
-    if (EXITFLAG==-2) % If EXITFLAG = -2, no feasible point was found. In this case, we set the antinorm of z equal to 0.
+    if (EXITFLAG==-2) % when EXITFLAG = -2, no feasible point was found. In this case, we set the antinorm of z equal to 0.
         lower = 0; upper = 0;
     
-    else % Otherwise (i.e., EXITFLAG = -3,...,-9), we print a message which gives the exact EXITFLAG, allowing us to handle the issue properly 
+    else % otherwise (i.e., EXITFLAG = -3,...,-9), we print a message which gives the exact EXITFLAG, allowing us to handle the issue properly 
         disp('Problems with the minimizer. Exit flag: '),disp(EXITFLAG);
         return
     end
     
 end
 
-if isempty(x)
+if isempty(x) % if no solution was found, return 0
     lower=0; upper=0;
     return
 end
