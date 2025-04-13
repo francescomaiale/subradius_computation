264ecee6c2c4648784f9a429b9c84943675998c1
Francesco Paolo Maiale <53312837+francescomaiale@users.noreply.github.com>
2025-04-14 00:27:08 +0200

    Update matrix_antinorm.m

--- matrix_antinorm.m
+++ matrix_antinorm.m
@@ -1,42 +1,31 @@
-function [anorm,z] = matrix_antinorm(A,V)
+function [a, v] = aNorm(Y, V)
+    % aNorm  Compute the "antinorm" of a matrix V under transformation Y.
+    %
+    % Inputs:
+    %   Y : an (n x n) or (n x k) matrix
+    %   V : an (n x j) matrix whose columns are vectors for which the antinorm is computed
+    %
+    % Outputs:
+    %   a : the minimum "upper" value of real_antinorm(V, Y*V(:,i)) over all columns i
+    %   v : the vector Y * V(:,I) that achieves this minimum
 
-% This function (Algorithm SM.1.2 in the supplementary material) computes the polytope antinorm (corresponding to the vertex set V) of the matrix A.
-% The functon real_antinorm (corresponding to Algorithm SM.1.1) is called here to find the polytope antinorm of vectors.
+    j = size(V, 2);      % number of columns (vectors) in V
+    c = zeros(j, 1);     % preallocate array for the "upper" values
 
-
-%% Input
-% A is a dxd real matrix for which we want to compute the antinorm
-% V is a dxp real matrix which contains the p vertices defining the polytope antinorm as columns
-
-
-%% Output
-% anorm = antinorm of the matrix A
-% z is the candidate new vertex in Algorithm A (adaptive_subradius_comp.m) and Algorithm E (adaptive_eigenvectors_subradius_comp.m)
-% and coincides with A*v_i, where v_i is the column of V achieving the minimum in the computation of the antinorm
-
-
-    
-p = size(V,2); % number of vertices
-c = zeros(p,1); % auxiliary vector that stores the antinorm of A*v_i for all i = 1,...,p
-
-for i = 1:p
-    
-    try
-        
-    [~,upper,~] = real_antinorm(V,A*V(:,i)); % compute the antinorm of A*v_i
-    c(i,1) = upper; % store the value in the auxiliary vector
-    
-    catch ME % if there is any error, print a message and skip to the next iteration
-        
-        fprintf('Error on iteration %d: %s\n', i, ME.message);
-        continue;
-        
+    for i = 1 : j
+        try
+            [lower, ~, ~] = real_antinorm(V, Y * V(:, i));
+            c(i) = lower;
+        catch ME
+            % If real_antinorm fails for any reason, you can choose to skip or handle differently
+            fprintf('Error on iteration %d: %s\n', i, ME.message);
+            c(i) = Inf; 
+            continue;
+        end
     end
-    
-end
 
-[anorm,I] = min(c(:,1)); % compute the minimum value of a(A*v_i) with respect to i and denote by I the index such that min = a(A*v_I)
+    [a, I] = min(c);
 
-z = A*V(:,I); % candidate new vertex
+    v = Y * V(:, I);
 
 end
